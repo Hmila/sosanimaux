7e716df2782ad8926d757ce93e4e84a465367a3a
Hmila <abderrahmen.hmila@esprit.tn>
2014-02-22 20:47:33 +0100

    phase1

--- src/sos/animaux/connection/MyConnection.java
+++ src/sos/animaux/connection/MyConnection.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 public class MyConnection {
 
      private static final  String driver = "com.mysql.jdbc.Driver";
-   private static final  String url = "jdbc:mysql://localhost:3306/ds";
+   private static final  String url = "jdbc:mysql://localhost:3306/animal";
    private static final  String login = "root";
    private static final  String pwd = "";
    private static Connection con;
